89d121be7dafbff3f9708c976bd25a82d326fc0f
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-06-09 17:07:39 +0900

    feat: installing vitest and using it

---
+++ src/__tests__/setup.ts
@@ -0,0 +1,6 @@
+// https://medium.com/@nedopaka/setup-a-react-vite-project-with-typescript-prettier-vitest-2024-9bb6e919ac8f
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
